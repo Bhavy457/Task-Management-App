bb1cafdb788f570ffa542e8582ce68e2f75c3bc6
Bhavy457 <bhavyjain533@gmail.com>
2024-10-27 12:55:06 +0530

    Setup the MongoDB Databse and Connection

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const app = express();
+
+// connectDB();
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+startServer();
