f43e7faeb88439d64596a0557888849e640714fb
Bhavy457 <bhavyjain533@gmail.com>
2024-10-27 22:58:41 +0530

    Created Routes for Create and Get Tasks

---
+++ Backend/routes/taskRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const Task = require("../model/taskModel");
+const { createTask, getTasks } = require("../controllers/taskController");
+const router = express.Router();
+
+//Create a Task
+router.post("/api/tasks", createTask);
+
+//Get/Read Data
+router.get("/api/tasks", getTasks);
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,17 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
-const connectDB = require("./config/connectDB");
+// const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
+const taskRoutes = require("./routes/taskRoute");
+
 const app = express();
 
 // connectDB();
+
+//Middleware
+app.use(express.json());
+app.use(taskRoutes);
 
 //Routes
 app.get("/", (req, res) => {
@@ -12,14 +20,25 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-  try {
-    await connectDB();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
-  } catch (error) {
-    console.log(error);
-  }
-};
-startServer();
+  })
+  .catch((err) => console.log(err));
+
+// Use this function in server,js to connect to MongoDB and start the server
+
+// const startServer = async () => {
+//   try {
+//     await connectDB();
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// startServer();
